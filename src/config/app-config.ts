acbf740f4da9a8e94812ed168e4b0abe3adb2c9f
Ali Zafar <alizafar@Alis-MacBook-Pro.local>
2020-10-01 07:41:11 +0500

    revert timeout testing scenario

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,7 +1,7 @@
 const BASE_URL = `https://gxxph4h9l6.execute-api.us-east-1.amazonaws.com/default/front_end_hiring`;
 const PROXY_SERVER_URL = 'https://cors-anywhere.herokuapp.com/';
 const NumberOfAPICallRetryAttempt = 2;
-const APITimeoutDuration = 4e3; // In milliseconds
+const APITimeoutDuration = 14e3; // In milliseconds
 
 const AppConfig = {BASE_URL, NumberOfAPICallRetryAttempt, APITimeoutDuration, PROXY_SERVER_URL};
 
